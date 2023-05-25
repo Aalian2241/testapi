30276afb2d8118999062a0caec5544df30d632c9
Aalian2241 <aalian.2241@gmail.com>
2023-05-25 19:45:23 +0500

    s

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [text, setText] = useState('');
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    setInputValue(text);
+  }, [text]);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleButtonClick = async () => {
+    try {
+      setText(inputValue);
+
+      console.log(inputValue)
+      const response = await axios.post('http://localhost:3000/download/youtube', { text: inputValue });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleButtonClick}>Send Request</button>
     </div>
   );
-}
+};
+
 
 export default App;
